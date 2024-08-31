67f2fc66c3e44977fafe73ff5d32a138d51ea31a
KlintLee115 <54990359+KlintLee115@users.noreply.github.com>
2024-08-31 11:40:14 -0600

    Completed week 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return <div>
+        <h3>Klint Lee</h3>
+        <Link href="https://github.com/KlintLee115">Github Repo</Link>
+    </div>
+}
