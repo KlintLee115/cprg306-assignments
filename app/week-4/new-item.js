5f1c339ea1b2b280903d8ad27fe9e777e101ba6f
KlintLee115 <54990359+KlintLee115@users.noreply.github.com>
2024-08-31 12:42:02 -0600

    week-4

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -3,85 +3,32 @@
 import React, { useState } from 'react';
 
 export default function NewItem() {
-
-    const [name, setName] = useState('');
     const [quantity, setQuantity] = useState(1);
-    const [category, setCategory] = useState('produce');
-
-    const handleSubmit = () => {
-
-        alert(`Add Item:\nName: ${name}\nQuantity: ${quantity}\nCategory: ${category}`)
-
-        setName('');
-        setQuantity(1);
-        setCategory('');
-    }
 
     const increment = () => quantity < 20 && setQuantity(quantity + 1)
     const decrement = () => quantity > 1 && setQuantity(quantity - 1)
 
     return (
 
-        <form onSubmit={handleSubmit}  >
-            <div className="p-8 mx-auto w-1/2 text-black">
+        <form className='bg-white p-10 mt-10 flex flex-col gap-4 mx-auto w-1/2 text-black' >
+            <label className='font-bold pb-2'>
+                Quantity: {quantity}
+            </label>
 
-                <div className='bg-white p-10 flex flex-col gap-4' >
-                    <div>
-                        <label className='font-bold   pb-2  '>
-                            Name:
-                        </label>
-                        <input
-                            type="text"
-                            value={name}
-                            onChange={e => setName(e.target.value)}
-                            required
-                            className="w-full border bg-gray-800 text-white p-2 rounded-md"
-                        />
-                    </div>
-
-                    <div>
-
-                        <label className='font-bold pb-2'>
-                            Quantity: {quantity}
-                        </label>
-
-                        <div>
-                            <input
-                                type="button"
-                                value="-"
-                                onClick={decrement}
-                                className="border bg-gray-800 text-white p-2 rounded-md w-1/2" />
-                            <input
-                                type="button"
-                                value="+"
-                                onClick={increment}
-                                className="border bg-gray-800 text-white p-2 rounded-md w-1/2" />
-                        </div>
-                    </div>
-                    <div className='w-1/2 '>
-                        <label className=' text-gray-600 font-bold  '>
-                            Category:
-                            <select
-                                className="border bg-gray-800 text-white p-2 rounded-md"
-                                value={category} onChange={e => setCategory(e.target.value)}>
-                                <option value="produce">Produce</option>
-                                <option value="dairy">Dairy</option>
-                                <option value="Bakery">Bakery</option>
-                                <option value="Meat">Meat</option>
-                                <option value="Canned Goods">Canned Goods</option>
-                                <option value="Dry Goods">Dry Goods</option>
-                                <option value="Household">Household</option>
-
-                            </select>
-                        </label>
-                    </div>
-                </div>
-
-
-                <button type="submit" className="bg-blue-700 p-2 rounded-md text-white font-bold cursor-pointer w-full mt-5">Add Item</button>
-
+            <div>
+                <input
+                    type="button"
+                    value="-"
+                    onClick={decrement}
+                    disabled={quantity === 1}
+                    className="border bg-gray-800 text-white p-2 rounded-md w-1/2 disabled:bg-gray-500" />
+                <input
+                    type="button"
+                    value="+"
+                    onClick={increment}
+                    disabled={quantity === 20}
+                    className="border bg-gray-800 text-white p-2 rounded-md w-1/2 disabled:bg-gray-500" />
             </div>
         </form>
-
     )
 }
